7b60aab793eb76675f3c003936877255d5b400dc
Aaron Kollasch <awkollasch@gmail.com>
2020-04-03 00:56:42 -0400

    Update demo_calc_logprobs info

--- examples/demo_calc_logprobs.sh
+++ examples/demo_calc_logprobs.sh
@@ -4,4 +4,3 @@ calc_logprobs_seqs_fr --sess BLAT_ECOLX_v2_channels-48_rseed-11_19Aug16_0626PM.c
   --channels 48 --r-seed 11 --dropout-p 0.5 --num-samples 10 \
   --input input/BLAT_ECOLX_r24-286_Ranganathan2015.fa \
   --output output/demo_BLAT_ECOLX_r24-286_Ranganathan2015_rseed-11_channels-48_dropoutp-0.5.csv
-
