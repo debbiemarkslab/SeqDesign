fe76f9b082265a8ef2975c5efa2bd1930f1113ba
Aaron Kollasch <awkollasch@gmail.com>
2020-04-02 23:53:30 -0400

    Improve installation

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-# Example installation script from scratch on Linux
+# Example installation script of SeqDesign for Tensorflow-GPU from scratch on Linux
 # Miniconda and Tensorflow 1.12 are installed here, but a working Tensorflow 1 environment can substitute.
 # Before running this script, first run `git clone https://github.com/debbiemarkslab/SeqDesign.git`
 # and then `cd SeqDesign`
+# If NVIDIA drivers have not been installed before, this script must be run twice, rebooting the system in between.
 
-#sudo apt update
-#sudo apt install -y --no-install-recommends nvidia-driver-430
-## Reboot.
+if [ ! -f "/proc/driver/nvidia/version" ]; then
+  sudo apt update
+  sudo apt install -y --no-install-recommends nvidia-driver-430
+  echo "Please reboot your system, then run linux_setup a second time."
+  exit
+  # Reboot.
+fi
 
 # set up conda and the SeqDesign environment
 wget https://repo.anaconda.com/miniconda/Miniconda2-latest-Linux-x86_64.sh
 sh Miniconda2-latest-Linux-x86_64.sh -b -p ~/miniconda2
-~/miniconda2/bin/conda init
-bash
+$HOME/miniconda2/bin/conda init
+. $HOME/.bashrc
 conda create -n seqdesign -y python=2.7 "tensorflow-gpu=1.12" scipy scikit-learn
 conda activate seqdesign
 python -c "from tensorflow.python.client import device_lib; print device_lib.list_local_devices()"  # test GPU install
