d0c9af16deb83893a97a510b78776e8f0571bfdf
Aaron Kollasch <awkollasch@gmail.com>
2020-04-03 00:04:24 -0400

    Improve installation

--- linux_setup.sh
+++ linux_setup.sh
@@ -18,19 +18,19 @@ wget https://repo.anaconda.com/miniconda/Miniconda2-latest-Linux-x86_64.sh
 sh Miniconda2-latest-Linux-x86_64.sh -b -p ~/miniconda2
 rm Miniconda2-latest-Linux-x86_64.sh
 $HOME/miniconda2/bin/conda init
-. $HOME/.bashrc
-conda create -n seqdesign -y python=2.7 "tensorflow-gpu=1.12" scipy scikit-learn
-conda activate seqdesign
-python -c "from tensorflow.python.client import device_lib; print device_lib.list_local_devices()"  # test GPU install
+$HOME/miniconda2/bin/conda create -n seqdesign -y python=2.7 "tensorflow-gpu=1.12" scipy scikit-learn
+$HOME/miniconda2/bin/python -c "from tensorflow.python.client import device_lib; print device_lib.list_local_devices()"  # test GPU install
 
 # download SeqDesign code
 # git clone https://github.com/debbiemarkslab/SeqDesign.git
 # cd SeqDesign || exit
-python setup.py install  # use setup.py develop if you want to modify the code files
+$HOME/miniconda2/bin/python setup.py install  # use setup.py develop if you want to modify the code files
 
 # download demo/example data
 cd examples || exit
 ./download_example_data.sh
+
+echo "SeqDesign installed. Run 'source ~/.bashrc; conda activate seqdesign' before using."
 
 # # to run training demo:
 # ./demo_train.sh
