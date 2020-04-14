28b2183f25f3b14ed04fcb922dbd7800353cdeda
Aaron Kollasch <awkollasch@gmail.com>
2020-04-14 19:41:39 -0400

    Update README.md and referenced files

--- README.md
+++ README.md
@@ -1,9 +1,47 @@
-# SeqDesign 
+# SeqDesign
+
+SeqDesign is a generative, unsupervised model for biological sequences.
+It is capable of learning functional constraints from unaligned sequences
+in order to predict the effects of mutations and generate novel sequences,
+including insertions and deletions. 
+For more information, check out the [biorxiv preprint](https://doi.org/10.1101/757252).
+
+This version of the codebase is compatible with Python 3 and Tensorflow 1. 
+For the Python 2.7 version used in the preprint, see the 
+[v2 branch](https://github.com/debbiemarkslab/SeqDesign/tree/v2). 
 
 ## Installation
 
-### Used packages and their versions:
-- tensorflow - Apache License
-- numpy - New BSD License
-- scipy - New BSD License
-- sklearn - New BSD License
+See [INSTALL.md](INSTALL.md).
+
+## Examples
+
+See the [examples](examples) directory.
+
+
+## Usage
+
+### Training
+
+Given a fasta file of training sequences, run:
+```shell script
+run_autoregressive_fr <your_dataset>.fa
+```
+
+### Mutation effect prediction
+Deterministic:
+```shell script
+calc_logprobs_seqs_fr --sess <your_sess> --dropout-p 1.0 --num-samples 1 --input <input>.fa --output <output>.csv
+```
+
+Average of 500 samples:
+```shell script
+calc_logprobs_seqs_fr --sess <your_sess> --dropout-p 0.5 --num-samples 500 --input <input>.fa --output <output>.csv
+```
+
+### Sequence generation
+```shell script
+generate_sample_seqs_fr --sess <your_sess>
+```
+
+Run each script with the `-h` argument to see additional arguments.
---
+++ linux_setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Example installation script of SeqDesign for Tensorflow-GPU from scratch
+# Tested on Ubuntu 18.04 LTS, runtime ~5 minutes including a reboot.
+# Miniconda and Tensorflow 1.12 are installed here, but a working Tensorflow 1 environment can substitute.
+# Before running this script, first run `git clone -b v3 https://github.com/debbiemarkslab/SeqDesign.git`
+# and then `cd SeqDesign`
+# If NVIDIA drivers have not been installed before, this script must be run twice, rebooting the system in between.
+
+if [ ! -f "/proc/driver/nvidia/version" ]; then
+  echo "NVIDIA driver not found; installing."
+  sudo apt update
+  sudo apt install -y --no-install-recommends nvidia-driver-430
+  echo "
+NVIDIA drivers installed.
+Please reboot your system, then run linux_setup.sh a second time."
+  exit
+fi
+
+# set up conda and the SeqDesign environment
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sh Miniconda3-latest-Linux-x86_64.sh -b -p "$HOME"/miniconda3
+rm Miniconda3-latest-Linux-x86_64.sh
+"$HOME"/miniconda3/bin/conda init
+"$HOME"/miniconda3/bin/conda create -n seqdesign -y python=3.7 "tensorflow-gpu=1.12" scipy scikit-learn
+"$HOME"/miniconda3/envs/seqdesign/bin/python -c "from tensorflow.python.client import device_lib; print device_lib.list_local_devices()"  # test GPU install
+
+# download SeqDesign code
+# git clone -b v3 https://github.com/debbiemarkslab/SeqDesign.git
+# cd SeqDesign || exit
+"$HOME"/miniconda3/envs/seqdesign/bin/python setup.py install  # use setup.py develop if you want to modify the code files
+
+# download demo/example data
+cd examples || exit
+./download_example_data.sh
+
+echo "
+SeqDesign installed.
+Run 'source ~/.bashrc; conda activate seqdesign' before using."
+
+# # to run training demo:
+# ./demo_train.sh
+
+# # to run calc_logprobs using trained weights:
+# ./demo_calc_logprobs.sh
+
+# # to generate sequences:
+# ./demo_generate.sh
