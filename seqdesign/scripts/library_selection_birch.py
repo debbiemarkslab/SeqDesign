9751d2a964032201ec48a5e32c01a567875b8d95
Aaron Kollasch <awkollasch@gmail.com>
2020-08-26 12:08:14 -0400

    Update library_selection_birch.py

--- seqdesign/scripts/library_selection_birch.py
+++ seqdesign/scripts/library_selection_birch.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
 from seqdesign import birch
 import sys
+import argparse
 
 
-# TODO test this script
-input_filename = '/n/groups/marks/users/adam/nanobody/generation/generate_test_sequences/process_output_sequences/make_feature_matrix/nanobody_id80_temp-1.0_param-nanobody.ckpt-250000unique_nanobodies_feat_matrix.csv'
-def main(input_filename):
-    # Hyperparameters
-    threshold = 0.575
-    branching_factor = 1000
-    minibatch_size = 1000
+def main():
+    parser = argparse.ArgumentParser(description="Make a feature matrix for BIRCH clustering from a fasta file.")
+    parser.add_argument("--input", type=str, required=True,
+                        help="Input feature matrix csv.")  # '/n/groups/marks/users/adam/nanobody/generation/generate_test_sequences/process_output_sequences/make_feature_matrix/nanobody_id80_temp-1.0_param-nanobody.ckpt-250000unique_nanobodies_feat_matrix.csv'
+    parser.add_argument("--output-prefix", type=str, required=True,
+                        help="Output cluster prefix.")  # 'nanobody_id80_temp-1.0_param-nanobody_18Apr18_1129PM.ckpt-250000unique_nanobodies_feat_matrix.csv'
+    parser.add_argument("--threshold", type=float, default=0.575, help="Birch threshold.")
+    parser.add_argument("--branching-factor", type=int, default=1000, help="Birch branching factor.")
+    parser.add_argument("--batch-size", type=int, default=1000, help="Birch batch size.")
+    parser.add_argument("--r-seed", type=int, default=1000, help="Birch branching factor.")
+    args = parser.parse_args()
 
-    data_helper = birch.NanobodyDataBirchCluster(input_filename=input_filename,
-        minibatch_size=minibatch_size, r_seed=43)
+    data_helper = birch.NanobodyDataBirchCluster(
+        input_filename=args.input,
+        minibatch_size=args.minibatch_size,
+        r_seed=args.r_seed)
 
-    birch_inst = birch.BirchIter(threshold=threshold, branching_factor=branching_factor)
-
+    birch_inst = birch.BirchIter(threshold=args.threshold, branching_factor=args.branching_factor)
     birch_inst.fit(data_helper)
 
-    output_name = "nanobody_id80_full_birch_thresh-"+str(threshold)+"_branch-"+str(branching_factor)+"_num_clusters-"+str(birch_inst.num_clusters)+".csv"
+    output_name = (
+        f"{args.output_prefix}_birch_thresh-{args.threshold}_branch-{args.branching_factor}_"
+        f"num_clusters-{birch_inst.num_clusters}.csv"
+    )
 
     print("\nPREDICTING LABELS\n")
 
-    birch_inst.predict(data_helper, minibatch_size=minibatch_size, output_name=output_name)
+    birch_inst.predict(data_helper, minibatch_size=args.batch_size, output_name=output_name)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        main(sys.argv[1])
-    else:
-        print("python library_selection_birch.py <input_filename>")
+    main()
