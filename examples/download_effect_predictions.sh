8e29836559bcca615da36d6ce5aba54c265d6525
Aaron Kollasch <awkollasch@gmail.com>
2021-03-15 18:00:33 -0400

    Add sequences and predictions to examples

---
+++ examples/download_effect_predictions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -o predictions.tar.gz https://marks.hms.harvard.edu/seqdesign/predictions.tar.gz
+tar -xzvf predictions.tar.gz
+rm predictions.tar.gz
---
+++ examples/download_generated_nanobodies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -o generated_nbs.tar.gz https://marks.hms.harvard.edu/seqdesign/generated_nbs.tar.gz
+tar -xzvf generated_nbs.tar.gz
+rm generated_nbs.tar.gz
