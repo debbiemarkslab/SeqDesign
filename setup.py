5d826c35060e78adbabc878e4d001a2fae4983ea
Aaron Kollasch <awkollasch@gmail.com>
2021-03-17 01:21:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,18 +7,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="seqdesign",
-    version="0.0.1",
+    version="1.0.0",
     author="Aaron Kollasch",
     author_email="awkollasch@gmail.com",
-    description="Biological sequence design for antibodies using deep learning",
+    description="Protein design and variant prediction using autoregressive generative models",
     license="MIT",
-    keywords="autoregressive protein sequence design deep learning",
+    keywords="autoregressive protein sequence design deep learning generative",
     url="https://github.com/debbiemarkslab/seqdesign",
     packages=setuptools.find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
